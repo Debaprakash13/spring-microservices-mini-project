170fe38b8894d676f460bb7e55c40beb723ff9ae
DEBAPRAKASH <debaprakash.biswal31@gmail.com>
2021-11-15 18:29:57 +0200

    initial commit

---
+++ src/test/java/com/devIn/rest/webservices/SpringMicroservicesMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devIn.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
